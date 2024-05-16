f342cf118b28ae444f8aa67a73a2850bbcf544c8
Harcharan singh <harcharansingh@Harcharans-MacBook-Pro.local>
2024-05-16 18:37:45 +0530

    Working on tables migration for my sql

--- lua/ms-tables.lua
+++ lua/ms-tables.lua
@@ -2,16 +2,16 @@ local connection = require "connection"
 local cjson = require "cjson"
 local helper = require "helper-functions"
 
-local pgTables = {}
+local msTables = {}
 
-function pgTables.create(args, isMigration)
-    local DB = connection.connectPgsql()
+function msTables.create(args, isMigration)
+    local DB = connection.connectMysql()
     if DB then
         local tableName = args.table
         local isDir, tableFile, timestamp = false, "", os.time()
         if not isMigration then
-            tableFile = "/opt/nginx/data/migrations/pgsql/" .. timestamp .. "-create_table_" .. tableName .. ".json"
-            isDir = helper.create_directory_if_not_exists("/opt/nginx/data/migrations/pgsql")
+            tableFile = "/opt/nginx/data/migrations/msql/" .. timestamp .. "-create_table_" .. tableName .. ".json"
+            isDir = helper.create_directory_if_not_exists("/opt/nginx/data/migrations/msql")
         end
         if isDir or isMigration then
             local file, fileErr = false, ""
@@ -39,13 +39,13 @@ function pgTables.create(args, isMigration)
                         createQuery = createQuery .. " DEFAULT " .. column.default
                     end
                     if column.auto_increment then
-                        createQuery = createQuery .. " SERIAL"
+                        createQuery = createQuery .. " AUTO_INCREMENT"
                     end
                     if column.primary then
-                        createQuery = createQuery .. " PRIMARY KEY"
+                        createQuery = createQuery .. " PRIMARY KEY (" .. column.name .. ")"
                     end
                     if column.unique then
-                        createQuery = createQuery .. " UNIQUE"
+                        createQuery = createQuery .. " UNIQUE (" .. column.name .. ")"
                     end
                     if column.not_null then
                         createQuery = createQuery .. " NOT NULL"
@@ -116,13 +116,13 @@ function pgTables.create(args, isMigration)
     end
 end
 
-function pgTables.alter(args, table, isMigration)
-    local DB = connection.connectPgsql()
+function msTables.alter(args, table, isMigration)
+    local DB = connection.connectMysql()
     if DB then
         local tableName, isDir, tableFile, timestamp = args.table, false, "", os.time()
         if not isMigration then
-            tableFile = "/opt/nginx/data/migrations/pgsql/" .. timestamp .. "-alter_table_" .. table .. ".json"
-            isDir = helper.create_directory_if_not_exists("/opt/nginx/data/migrations/pgsql")
+            tableFile = "/opt/nginx/data/migrations/msql/" .. timestamp .. "-alter_table_" .. table .. ".json"
+            isDir = helper.create_directory_if_not_exists("/opt/nginx/data/migrations/msql")
         end
         if isDir or isMigration then
             local file, fileErr = false, ""
@@ -152,13 +152,13 @@ function pgTables.alter(args, table, isMigration)
                             createQuery = createQuery .. " DEFAULT " .. column.default
                         end
                         if column.auto_increment then
-                            createQuery = createQuery .. " SERIAL"
+                            createQuery = createQuery .. " AUTO_INCREMENT"
                         end
                         if column.primary then
-                            createQuery = createQuery .. " PRIMARY KEY"
+                            createQuery = createQuery .. " PRIMARY KEY (" .. column.name .. ")"
                         end
                         if column.unique then
-                            createQuery = createQuery .. " UNIQUE"
+                            createQuery = createQuery .. " UNIQUE (" .. column.name .. ")"
                         end
                         if column.not_null then
                             createQuery = createQuery .. " NOT NULL"
@@ -186,32 +186,28 @@ function pgTables.alter(args, table, isMigration)
                 end
                 if args.clause == "alter_column" then
                     for i, column in ipairs(args.columns) do
-                        createQuery = createQuery .. "ALTER COLUMN " .. column.name
+                        createQuery = createQuery .. "MODIFY COLUMN " .. column.name
                         if column.type then
-                            createQuery = createQuery .. " SET DATA TYPE " .. column.type
+                            createQuery = createQuery .. " " .. column.type
                         end
                         if column.default ~= "" then
-                            createQuery = createQuery .. ", ALTER COLUMN " .. column.name
-                            createQuery = createQuery .. " SET DEFAULT " .. column.default
+                            createQuery = createQuery .. ", MODIFY COLUMN " .. column.name
+                            createQuery = createQuery .. " DEFAULT " .. column.default
                         end
                         if column.auto_increment then
-                            createQuery = createQuery .. " SERIAL"
+                            createQuery = createQuery .. " AUTO_INCREMENT"
                         end
                         if column.primary then
-                            createQuery = createQuery .. ", ALTER COLUMN " .. column.name
+                            createQuery = createQuery .. ", MODIFY COLUMN " .. column.name
                             createQuery = createQuery .. " ADD PRIMARY KEY (" .. column.name .. ")"
                         end
                         if column.unique then
-                            createQuery = createQuery .. ", ALTER COLUMN " .. column.name
+                            createQuery = createQuery .. ", MODIFY COLUMN " .. column.name
                             createQuery = createQuery .. " ADD UNIQUE (" .. column.name .. ")"
                         end
                         if column.not_null then
-                            createQuery = createQuery .. ", ALTER COLUMN " .. column.name
-                            createQuery = createQuery .. " SET NOT NULL"
-                        end
-                        if column.not_null == false then
-                            createQuery = createQuery .. ", ALTER COLUMN " .. column.name
-                            createQuery = createQuery .. " DROP NOT NULL"
+                            createQuery = createQuery .. ", MODIFY COLUMN " .. column.name
+                            createQuery = createQuery .. " NOT NULL"
                         end
                         if i < #args.columns then
                             createQuery = createQuery .. ", "
@@ -252,14 +248,14 @@ function pgTables.alter(args, table, isMigration)
     end
 end
 
-function pgTables.drop(args, isMigration)
-    local DB = connection.connectPgsql()
+function msTables.drop(args, isMigration)
+    local DB = connection.connectMysql()
     if DB then
         for _i, table in ipairs(args.tables) do
-            local tableFile, isDir, timestamp = "", false, os.time()
+            local tableFile, isDir = "", false
             if not isMigration then
-                tableFile = "/opt/nginx/data/migrations/pgsql/" .. timestamp .. "-delete_table_" .. table.name .. ".json"
-                isDir = helper.create_directory_if_not_exists("/opt/nginx/data/migrations/pgsql")
+                tableFile = "/opt/nginx/data/migrations/msql/" .. os.time() .. "-delete_table_" .. table.name .. ".json"
+                isDir = helper.create_directory_if_not_exists("/opt/nginx/data/migrations/msql")
             end
             if isDir or isMigration then
                 local file, fileErr = false, ""
@@ -291,18 +287,6 @@ function pgTables.drop(args, isMigration)
                             status = 500
                         }))
                     end
-                    local fileName = isMigration and isMigration or timestamp .. "-delete_table_" .. table .. ".json"
-                    local insertQuery = "INSERT INTO migrations (name, created_at) VALUES ('" .. fileName .. "', NOW());"
-                    local insertRes, insertErr = DB:query(insertQuery)
-                    if not insertRes then
-                        ngx.say(cjson.encode({
-                            data = {
-                                message = "Error executing SQL query while inserting into migration",
-                                error = insertErr
-                            },
-                            status = 500
-                        }))
-                    end
                 end
             end
         end
@@ -315,42 +299,51 @@ function pgTables.drop(args, isMigration)
     end
 end
 
-function pgTables.migrate()
-    local DB = connection.connectPgsql()
-    local directory = "/opt/nginx/data/migrations/pgsql"
-    local files = helper.get_files_in_directory(directory)
-    local unMigrated = {}
-    for _i, migration in ipairs(files) do
-        local query = string.format("SELECT id, name FROM migrations WHERE name='%s'", migration)
-        local res, err, num = DB:query(query)
-        if not res then
-            ngx.say("Error executing SQL query: ", err)
-            ngx.exit(500)
+function msTables.migrate()
+    local DB = connection.connectMysql()
+    if DB then
+        local directory = "/opt/nginx/data/migrations/msql"
+        local files = helper.get_files_in_directory(directory)
+        local unMigrated = {}
+        for _i, migration in ipairs(files) do
+            local query = string.format("SELECT id, name FROM migrations WHERE name='%s'", migration)
+            local res, err, num = DB:query(query)
+            if not res then
+                ngx.say("Error executing SQL query: ", err)
+                ngx.exit(500)
+            end
+            if next(res) == nil then
+                table.insert(unMigrated, migration)
+            end
         end
-        if next(res) == nil then
-            table.insert(unMigrated, migration)
+        local unMigratedFiles = helper.sort_files_by_timestamp(unMigrated)
+        local response = {}
+        for i, file in ipairs(unMigratedFiles) do
+            local filePath = "/opt/nginx/data/migrations/msql/" .. file
+            local migrationFile = helper.read_file(filePath)
+            local isCreate = helper.contains(file, "create_table")
+            local isAlter = helper.contains(file, "alter_table")
+            local isDelete = helper.contains(file, "delete_table")
+            local payloads = cjson.decode(migrationFile)
+            local table = payloads.table
+            if isCreate then
+                response = msTables.create(payloads, file)
+            elseif isAlter then
+                response = msTables.alter(payloads, table, file)
+            elseif isDelete then
+                response = msTables.drop(payloads, file)
+            end
+    
         end
+        ngx.say(response)
+    else
+        ngx.say(cjson.encode({
+            data = {
+                message = "Error connecting to database"
+            },
+            status = 500
+        }))
     end
-    local unMigratedFiles = helper.sort_files_by_timestamp(unMigrated)
-    local response = {}
-    for i, file in ipairs(unMigratedFiles) do
-        local filePath = "/opt/nginx/data/migrations/pgsql/" .. file
-        local migrationFile = helper.read_file(filePath)
-        local isCreate = helper.contains(file, "create_table")
-        local isAlter = helper.contains(file, "alter_table")
-        local isDelete = helper.contains(file, "delete_table")
-        local payloads = cjson.decode(migrationFile)
-        local table = payloads.table
-        if isCreate then
-            response = pgTables.create(payloads, file)
-        elseif isAlter then
-            response = pgTables.alter(payloads, table, file)
-        elseif isDelete then
-            response = pgTables.drop(payloads, file)
-        end
-
-    end
-    ngx.say(response)
 end
 
-return pgTables
+return msTables
